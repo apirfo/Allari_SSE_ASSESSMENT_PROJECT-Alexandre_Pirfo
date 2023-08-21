a6d157b4b1bc2e88667a547fe43181be0c629f62
alexandrepirfo <alexandre.pirfo@biomtech.com.br>
2023-08-21 16:33:52 -0300

    Add project files.

---
+++ Allari_SSE_ASSESSMENT_PROJECT-Alexandre_Pirfo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Allari_SSE_ASSESSMENT_PROJECT_Alexandre_Pirfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Allari_SSE_ASSESSMENT_PROJECT-Alexandre_Pirfo/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    setInterval(function time(){
+        let dateNow = new Date();
+
+        console.log('oi');
+
+        $("#clockHeaderHours").html(dateNow.getHours().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }));
+        $("#clockHeaderMinuts").html(dateNow.getMinutes().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }));
+        $("#clockHeaderSeconds").html(dateNow.getSeconds().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }));
+    });
+
+});
+
+let mousePos = { x: undefined, y: undefined };
+
+window.addEventListener('mousemove', (event) => {
+    mousePos = { x: event.clientX, y: event.clientY };
+    $("#mouseXPosition").html(mousePos.x);
+    $("#mouseYPosition").html(mousePos.y);
+});
